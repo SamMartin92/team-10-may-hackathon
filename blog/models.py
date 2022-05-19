5d5ab325bd9e58716034ec7e7b444d6a74e15126
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-05-19 22:33:21 +0000

    Add post and comments models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,45 @@
+""" Imports for blog app models"""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
+class Post(models.Model):
+    """ Creates blog table in database"""
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField(null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        """ Displays posts, newest first """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """ Override default str method """
+        return self.title
+
+
+class Comments(models.Model):
+    """ Creates comments table in database"""
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """ Displays comments, newest first """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """ Override default str method """
+        return self.content
