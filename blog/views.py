1e5187e9dfb3fb1e08d7b234090c5fd2a0229811
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-05-19 22:33:55 +0000

    Start blog views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
+""" Imports required by blog app """
 from django.shortcuts import render
+from .models import Post, Comments
 
-# Create your views here.
+
+def blog_posts(request):
+    """ Get blog posts """
+    all_posts = Post.objects.all()
+    all_comments = Comments.objects.all()
+    context = {
+        'posts': all_posts,
+        'comments': all_comments,
+    }
+
+    return render(request, 'blog/blog.html', context)
