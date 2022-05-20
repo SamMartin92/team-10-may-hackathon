a54e3cce158319c8f00cad60583507f94a3a9eb6
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-05-20 23:15:05 +0000

    Add more url paths

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('blog/', views.blog_posts, name='blog'),
+    path('create_post/', views.create_post, name='create_post'),
     path('<slug:slug>/', views.post_details, name='post_detail'),
     path('edit_blog/<int:post_id>/', views.edit_blog, name='edit_blog'),
     path('delete_blog/<int:post_id>/', views.delete_blog, name='delete_blog'),
+    path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
 ]
