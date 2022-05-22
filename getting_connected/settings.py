76419d836cdbfe67fb53ac8d6820320618bb6054
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2022-05-22 19:34:16 +0000

    Add static_root

--- getting_connected/settings.py
+++ getting_connected/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
