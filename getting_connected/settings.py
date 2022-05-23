24e3dc45987f1e14773cd2f8a0eccc1f69918f85
SamMartin92 <samm92@live.ie>
2022-05-23 11:46:22 +0000

    adds some styling to resources hero image

--- getting_connected/settings.py
+++ getting_connected/settings.py
@@ -33,7 +33,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
